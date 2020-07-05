59e2a8658cc3eacee88b56a5a7c457285a2ffd7e
Suddala Srujan <srujan35007@gmail.com>
2020-07-05 21:29:52 +0530

    Initial commit

---
+++ auto_encoders_TF.py
@@ -0,0 +1,7 @@
+from tensorflow.keras import models, datasets
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, Dropout, BatchNormalization
+import numpy as np 
+import matplotlib.pyplot as plt 
+import math
+import time 
+from tqdm.notebook import tqdm
