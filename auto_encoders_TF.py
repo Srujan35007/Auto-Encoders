5c517f160a03d908db753ef11433f93cbde9d5f0
Suddala Srujan <srujan35007@gmail.com>
2020-07-06 09:31:10 +0530

    OOP model needed

--- auto_encoders_TF.py
+++ auto_encoders_TF.py
@@ -9,6 +9,7 @@ from tensorflow.keras import models, datasets
 aft = time.time()
 print(f'Imports complete in {aft-bef} seconds')
 
+# Encoder
 encoder = models.Sequential()
 encoder.add(Conv2D(32, kernel_size=(2, 2),
                    activation='sigmoid', input_shape = (28,28,1)))
@@ -21,12 +22,19 @@ encoder.add(BatchNormalization())
 encoder.add(Dense(100, activation='relu'))
 encoder.add(Dropout(0.3))
 encoder.add(Dense(10, activation='softmax'))
-print(f'Model created.')
+print(f'Encoder created.')
 encoder.compile(loss = 'sparse_categorical_crossentropy',\
      optimizer='adam', metrics=['accuracy'])
-print('Model compiled.')
-#print(encoder.summary())
+print('Encoder compiled.')
 
+# Decoder
+decoder = models.Sequential()
+decoder.add(Dense(100, activation='relu'))
+decoder.add(Dense(100, activation='relu'))
+decoder.add(BatchNormalization())
+
+
+#--------------fitting and testing------------------
 (X_train, y_train), (X_test, y_test) = datasets.mnist.load_data()
 X_train = X_train/255
 X_test = X_test/255
